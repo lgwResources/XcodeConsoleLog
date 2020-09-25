abac6a9af0969200653b2d8dbcad1379fd2de77d
刘功武 <gongwu.liu@chetuobang.com>
2020-09-25 09:42:15 +0800

    控制台中文乱码

---
+++ NSArray+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Extension.h
+//  Extension
+//
+//  Created by 刘功武 on 2020/9/15.
+//  Copyright © 2020 . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Extension)
+
+@end
---
+++ NSDictionary+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSDictionary+Extension.h
+//  Extension
+//
+//  Created by 刘功武 on 2020/9/15.
+//  Copyright © 2020 . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (Extension)
+
+@end
